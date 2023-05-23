fddc7bb35ee3d0a1cd7cdaf595839ec6bccc0773
kosdmit <kosdmit@hotmail.com>
2023-05-23 12:28:46 +0400

    Added static_root parameter

--- CV_project/settings.py
+++ CV_project/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = STATIC_ROOT = "/var/www/svproject/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
